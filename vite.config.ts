b5bf38c28c554b70fdce82ce012a8f61cd44cd3b
LuN3cy <1536968883@qq.com>
2025-12-05 15:21:24 +0800

    Optimize loading speed and fixed related errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { imagetools } from 'vite-imagetools';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -10,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react(), imagetools()],
+      plugins: [react()],
       base: process.env.NODE_ENV === 'production' ? '/LuN3cy/' : '/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
